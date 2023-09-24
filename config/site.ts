d68fa37377c8505e3853d27fdddcd39549684cd7
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-09-24 14:50:16 -0400

    feat: add site config

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "Metawizard",
+  url: "https://metawizard.vercel.app/",
+  ogImage: "https://metawizard.vercel.app/og.jpg",
+  description:
+    "A web-based metadata generator application to assist users in generating and managing metadata for their Next.js 13 websites & applications.",
+  links: {
+    twitter: "https://twitter.com/jal_eelll",
+    github: "https://github.com/JaleelB/metawizard",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
