9587a5b8a0e94b8033fde404a3c08939f67cb2db
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-09-24 13:59:48 -0400

    misc: default exports for config forms

---
+++ components/steps/index.ts
@@ -0,0 +1,5 @@
+export { default as SiteConfigLayout } from "./site-config";
+export { default as SiteImgagesLayout } from "./site-images-config";
+export { default as AuthorConfigsLayout } from "./author-config";
+export { default as RobotsConfigLayout } from "./robots-config";
+export { default as SitemapConfigLayout } from "./sitemap-config";
