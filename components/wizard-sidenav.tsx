c07bf9e9a541dd38ccfdfb6a196159697335d8b6
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-10-30 11:14:54 -0400

    feat: made wizard nav collapsible

--- components/wizard-sidenav.tsx
+++ components/wizard-sidenav.tsx
@@ -1,51 +1,69 @@
-import Link from "next/link";
 import { useWizard } from "react-use-wizard";
 import { Icons } from "./ui/icons";
-import { siteConfig } from "@/config/site";
 import { type Step } from "@/config/steps-config";
+import React from "react";
+import { Button } from "./ui/button";
+import { useLocalStorage } from "@/hooks/use-local-storage";
 
 export default function WizardNav({ steps }: { steps: Step[] }) {
-  const { activeStep, stepCount } = useWizard();
+  const { activeStep } = useWizard();
+  const [isNavVisible, setIsNavVisible] = useLocalStorage({
+    key: "wizard-nav-visible",
+    defaultValue: true,
+  });
 
   return (
-    <aside className="hidden lg:block bg-[#F9FAFC] p-8 pl-10">
-      <div className="flex flex-col">
-        <Link href="/" className="pb-20 flex items-center space-x-2">
-          <Icons.logo className="h-8 w-8 text-primary" />
-          <span className="hidden font-bold text-xl sm:inline-block">
-            {siteConfig.name}
-          </span>
-        </Link>
-        <div className="flex flex-col gap-6">
-          {steps.map((step, index) => (
-            <div
-              key={index}
-              className={`flex gap-4 items-start relative text-left ${
-                steps[activeStep].title !== steps[index].title
-                  ? "opacity-30"
-                  : ""
-              }`}
-            >
-              <div className="h-5 w-5 pt-0.5">
-                <Icons.check className="h-5 w-5 text-primary" />
-              </div>
+    <aside className="relative">
+      <div
+        className={`h-full ${
+          isNavVisible ? "lg:block" : "lg:hidden"
+        } border-r px-6 py-12`}
+      >
+        <div className="flex flex-col">
+          <div className="flex flex-col gap-6">
+            {steps.map((step, index) => (
+              <div
+                key={index}
+                className={`flex gap-4 items-start relative text-left ${
+                  steps[activeStep].title !== steps[index].title
+                    ? "opacity-30"
+                    : ""
+                }`}
+              >
+                <div className="h-5 w-5 pt-0.5">
+                  <Icons.check className="h-5 w-5 text-primary" />
+                </div>
 
-              <div>
-                <h3
-                  className={`
+                <div>
+                  <h3
+                    className={`
                     text-left mb-1 font-semibold
                     ${steps[activeStep] === steps[index] ? "text-primary" : ""}
                 `}
-                >
-                  {step.title}
-                </h3>
-                <p className="text-sm text-muted-foreground">
-                  {step.description}
-                </p>
+                  >
+                    {step.title}
+                  </h3>
+                  <p className="text-sm text-muted-foreground">
+                    {step.description}
+                  </p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
+      </div>
+      <div
+        className={`absolute bottom-0 ${
+          isNavVisible ? "pl-6 w-[350px] -rotate-180" : "w-[50px] pl-6"
+        } left-0 z-10 flex h-20 items-center justify-start`}
+      >
+        <Button
+          onClick={() => setIsNavVisible(!isNavVisible)}
+          variant={"outline"}
+          className="hidden h-8 w-8 items-center justify-center px-0 lg:flex"
+        >
+          <Icons.chevronLeft className="w-4 h-4" />
+        </Button>
       </div>
     </aside>
   );
