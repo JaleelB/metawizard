0512b9bbf41977046ff5320f633ab903b6c71ee0
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-10-31 11:34:41 -0400

    misc: included manifest details in steps config

--- config/steps-config.ts
+++ config/steps-config.ts
@@ -7,7 +7,7 @@ export const steps = [
   {
     title: "Site Images",
     description:
-      "Upload or provide URLs for your site's logo and Open Graph image. You can also choose to auto-generate these images.",
+      "Define the visual identity of your website or application. You can also choose to auto-generate these images.",
   },
   {
     title: "Author Config",
@@ -15,14 +15,19 @@ export const steps = [
       "Provide author information including name and URLs to author's personal website, GitHub, and Twitter profiles.",
   },
   {
+    title: "Sitemap.xml",
+    description:
+      "Configure the sitemap.xml file to improve site crawling by search engines. ",
+  },
+  {
     title: "Robots.txt",
     description:
       "Configure the robots.txt file to control how search engines crawl and index your site.",
   },
   {
-    title: "Sitemap.xml",
+    title: "Manifest.json",
     description:
-      "Configure the sitemap.xml file to improve site crawling by search engines. Define your site's endpoints, change frequency, and priority.",
+      "Configure the manifest.json file to provide information about your site to browsers and devices.",
   },
 ];
 
