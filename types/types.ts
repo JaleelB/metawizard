121dab79a0ef2ef08cb6a0f24fdecea7b9dcb971
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-10-30 21:47:32 -0400

    feat: created schemas type

--- types/types.ts
+++ types/types.ts
@@ -1,3 +1,13 @@
+import { z } from "zod";
+import {
+  authorConfigSchema,
+  robotsConfigSchema,
+  siteConfigSchema,
+  siteImagesConfigSchema,
+  siteManifestConfigSchema,
+  sitemapConfigSchema,
+} from "@/schemas/schema";
+
 type ButtonProps = {
   navigateTo?: () => void;
   buttonText: string;
@@ -7,3 +17,11 @@ export type MultiStepNavigation = {
   prevButton?: ButtonProps;
   nextButton?: ButtonProps;
 };
+
+export type schemaTypes =
+  | z.infer<typeof siteConfigSchema>
+  | z.infer<typeof siteImagesConfigSchema>
+  | z.infer<typeof authorConfigSchema>
+  | z.infer<typeof robotsConfigSchema>
+  | z.infer<typeof sitemapConfigSchema>
+  | z.infer<typeof siteManifestConfigSchema>;
