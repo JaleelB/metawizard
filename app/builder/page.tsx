0d6e3d7513bb3c4f7775f6e700f27fca4e7f8c17
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-10-31 09:14:38 -0400

    fix: wrapping page in form preview provider

--- app/builder/page.tsx
+++ app/builder/page.tsx
@@ -9,13 +9,15 @@ import {
 import { Wizard } from "react-use-wizard";
 import { motion } from "framer-motion";
 import { steps } from "@/config/steps-config";
-import { useEffect } from "react";
+import WizardNav from "@/components/wizard-sidenav";
+import { useEffect, useState } from "react";
+import React from "react";
+import { FormProvider } from "@/state/form-state";
 import { useLocalStorage } from "@/hooks/use-local-storage";
 import { toast } from "@/components/ui/use-toast";
-import WizardNav from "@/components/wizard-sidenav";
 import SiteManifestConfigLayout from "@/components/form/site-manifest-config";
 
-export default function BuilderPage() {
+export default function ConfigurePage() {
   const [isNavVisible, setIsNavVisible] = useLocalStorage({
     key: "wizard-nav-visible",
     defaultValue: true,
@@ -25,12 +27,17 @@ export default function BuilderPage() {
     },
   });
 
+  const [gridTemplateColumns, setGridTemplateColumns] =
+    useState<string>("0px 1fr");
+
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth < 1440) {
         setIsNavVisible(false);
-      } else if (window.innerWidth > 1440) {
+        setGridTemplateColumns("1fr");
+      } else if (window.innerWidth >= 1440) {
         setIsNavVisible(true);
+        setGridTemplateColumns("350px 1fr");
       }
     };
 
@@ -44,30 +51,32 @@ export default function BuilderPage() {
   }, [setIsNavVisible]);
 
   return (
-    <motion.div
-      initial={{ gridTemplateColumns: "350px 1fr" }}
-      animate={{
-        gridTemplateColumns: isNavVisible ? "350px 1fr" : "0px 1fr",
-      }}
-      transition={{ duration: 0.5, ease: "easeInOut" }}
-      className={`h-[calc(100vh-65px)] w-full overflow-hidden grid grid-cols-1 flex-1 lg:grid-cols-[350px_1fr]`}
-    >
-      <Wizard
-        header={
-          <WizardNav
-            steps={steps}
-            isNavVisible={isNavVisible}
-            setIsNavVisible={setIsNavVisible}
-          />
-        }
+    <FormProvider>
+      <motion.div
+        initial={{ gridTemplateColumns: gridTemplateColumns }}
+        animate={{
+          gridTemplateColumns: isNavVisible ? "350px 1fr" : "0px 1fr",
+        }}
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+        className={`h-[calc(100vh-65px)] w-full overflow-hidden grid grid-cols-1 flex-1`}
       >
-        <SiteConfigLayout />
-        <SiteImgagesLayout />
-        <AuthorConfigsLayout />
-        <SitemapConfigLayout />
-        <RobotsConfigLayout />
-        <SiteManifestConfigLayout />
-      </Wizard>
-    </motion.div>
+        <Wizard
+          header={
+            <WizardNav
+              steps={steps}
+              isNavVisible={isNavVisible}
+              setIsNavVisible={setIsNavVisible}
+            />
+          }
+        >
+          <SiteConfigLayout />
+          <SiteImgagesLayout />
+          <AuthorConfigsLayout />
+          <SitemapConfigLayout />
+          <RobotsConfigLayout />
+          <SiteManifestConfigLayout />
+        </Wizard>
+      </motion.div>
+    </FormProvider>
   );
 }
