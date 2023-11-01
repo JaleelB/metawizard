01d04dcdc076a073453c249a0a7527ec1e0095b8
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-11-01 14:27:29 -0400

    misc: extending 2xl screen breakpoint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,9 @@ module.exports = {
       },
     },
     extend: {
+      screens: {
+        "2xl": "1440px",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
